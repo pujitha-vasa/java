7dbba9db1a3d0f1093e989cba36e4dec08cc0d2c
pujitha-vasa <151710754+pujitha-vasa@users.noreply.github.com>
2024-08-22 12:24:49 +0530

    Create Methodsaddition.java

---
+++ Methodsaddition.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+public class Main
+{
+    int x,y,z;
+    int add(int x,int y)
+    {
+        z=x+y;
+        return z;
+    }
+    public static void main(String agrs[])
+    {
+        int x,y,z;
+        Main a= new Main();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter an two integers for addition:");
+        x=sc.nextInt();
+        y=sc.nextInt();
+        z=a.add(x,y);
+        System.out.println("Sum of"+x+"and"+y+"is"+z);
+    }
+}
